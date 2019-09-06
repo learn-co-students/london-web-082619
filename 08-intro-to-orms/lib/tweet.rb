d9fd2bc6c4077fbd833d34d0f5da625ba17acd58
Sam Barker <samuel.e.barker@googlemail.com>
2019-09-06 09:19:10 +0100

    08 after lecture, 09 before lecture

--- 08-intro-to-orms/lib/tweet.rb
+++ 08-intro-to-orms/lib/tweet.rb
@@ -1,14 +1,65 @@
 class Tweet
-  attr_accessor :message, :username
-  ALL = []
+  attr_accessor :message, :username, :id
 
   def self.all
-    ALL
+    sql = "SELECT * FROM tweets;"
+
+    tweets_as_hashes = DB[:conn].execute(sql)
+
+    tweets_as_hashes.map {|t_hash| Tweet.new(t_hash)}
   end
 
   def initialize(props={})
     @message = props['message']
     @username = props['username']
-    ALL << self
+    @id = props['id']
+  end
+
+  def self.create(props={})
+    tweet = Tweet.new(props)
+    tweet.save
+    tweet
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM tweets WHERE id = ?"
+
+    results = DB[:conn].execute(sql, id)
+
+    Tweet.new(results.first)
+  end
+
+  def update(props={})
+    @message = props['message'] || @message
+    @username = props['username'] || @username
+    @id = props['id'] || @id
+    self.save
+  end
+
+  def save
+
+    if @id != nil
+      sql = "UPDATE tweets SET message = ?, username = ? WHERE id = ?;"
+
+      DB[:conn].execute(sql, self.message, self.username, self.id)
+      
+      self
+    else
+      sql = "INSERT INTO tweets(message, username) VALUES(?, ?);"
+
+      DB[:conn].execute(sql, self.message, self.username)
+      
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM tweets").first["last_insert_rowid()"]
+
+      self
+    end
+  end
+
+  def destroy
+    sql = 'DELETE FROM tweets WHERE id = ?;'
+
+    DB[:conn].execute(sql, self.id)
+
+    true
   end
 end
