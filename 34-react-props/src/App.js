1edc65db30f2e53ed1be8f125f37a42262e58f5a
Sam Barker <samuel.e.barker@googlemail.com>
2019-10-28 11:26:16 +0000

    react props before lecture

---
+++ 34-react-props/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './App.css'
+import artworks from './artworks'
+
+console.log(artworks)
+
+class App extends Component {
+  render() {
+    return <div className="App"></div>
+  }
+}
+
+export default App
